7690fd85c7da8b1823496bf8797e4d5d00d74cbd
MrSnakeDoc <selabbadi@gmail.com>
2022-04-27 15:10:34 +0200

    fix: removed almost all logs

--- src/app/router.ts
+++ src/app/router.ts
@@ -3,16 +3,14 @@ import { Request, Response, Router } from "express";
 const router: Router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-  console.log(req);
-
   const ip: string =
     req.header("x-forwarded-for") || req.connection.remoteAddress;
-  console.log("ip:", ip);
-
+  console.log(
+    "req.header('x-forwarded-for') : ",
+    req.header("x-forwarded-for")
+  );
   const language: string = req.header("Accept-Language");
-  console.log("language:", language);
   const system: string = req.header("User-Agent");
-  console.log("system:", system);
   res.json({
     ipaddress: ip,
     language,
